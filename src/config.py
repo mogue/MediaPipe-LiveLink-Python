d6612561aa278c51a5766947fd5d1be0dddcf6a9
mogue <mattimatti@gmail.com>
2024-02-29 21:15:20 +0000

    Initialize the WebCam_LiveLink.ini

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import configparser, os
 
-config_filepath = 'config.ini'
+config_filepath = 'WebCam_LiveLink.ini'
 config = configparser.ConfigParser()
 
 def save_config():
@@ -12,9 +12,32 @@ if os.path.exists(config_filepath):
     config.read(config_filepath)
 else:
     # Creata a default config.ini file.
+    config["UI"] = {
+        "preview_wireframe": "True",
+        "preview_camera": "True"
+    }
+
     config["UDP"] = {
         "IP": "127.0.0.1",
         "Port": "11111"
+    }
+
+    config["Camera"]  = {
+        "input_source": "0",
+        "FPS": "30",
+        "width": "0",
+        "height": "0"
+    }
+    
+    config["MediaPipe"] = {
+        "min_face_detection_confidence": "0.5",
+        "min_face_presence_confidence": "0.5",
+        "min_tracking_confidence": "0.5"
+    }
+
+    config["LiveLink"] = {
+        "device_id": "WebCam",
+        "subject_name": "BasicRole"
     }
 
     save_config()
